3091729aee08f12edd9adaff33f9967de0d6717c
joaocastro2 <joaocastrocntt06@gmail.com>
2025-02-13 22:13:15 -0300

    ---

--- CrudComSpring2/src/main/java/com/example/crud/controllers/ProductController.java
+++ CrudComSpring2/src/main/java/com/example/crud/controllers/ProductController.java
@@ -6,7 +6,10 @@ import com.example.crud.domain.product.RequestProduct;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/product")
@@ -28,4 +31,21 @@ public class ProductController {
         repository.save(newProduct);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if (optionalProduct.isPresent()){
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+
 }
--- CrudComSpring2/src/main/java/com/example/crud/domain/product/RequestProduct.java
+++ CrudComSpring2/src/main/java/com/example/crud/domain/product/RequestProduct.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RequestProduct(
+
+        String id,
         @NotBlank
         String name,
         @NotNull
