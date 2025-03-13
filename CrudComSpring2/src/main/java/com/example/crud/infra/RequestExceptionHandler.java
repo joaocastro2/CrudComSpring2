24976c185ca551f710b9c5b5def50087d76674d4
joaocastro2 <joaocastrocntt06@gmail.com>
2025-03-13 20:52:27 -0300

    criação de uma classe para tratar exceções

--- CrudComSpring2/src/main/java/com/example/crud/infra/RequestExceptionHandler.java
+++ CrudComSpring2/src/main/java/com/example/crud/infra/RequestExceptionHandler.java
@@ -1,4 +1,14 @@
 package com.example.crud.infra;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class RequestExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(){
+        return ResponseEntity.badRequest().body("Dado não encontrado");
+    }
 }
